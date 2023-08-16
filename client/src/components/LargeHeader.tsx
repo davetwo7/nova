f75d16ee5d59f0d588d152b3bf3710323d30206e
davetwo7 <davidolu2019@gmail.com>
2023-08-15 21:45:58 -0400

    Add interface for LargeHeader component

--- client/src/components/LargeHeader.tsx
+++ client/src/components/LargeHeader.tsx
@@ -1,9 +1,13 @@
-const LargeHeader = ({title}) => {
+interface LargeHeaderProps {
+  title: string;
+}
+
+const LargeHeader = ({ title }: LargeHeaderProps) => {
   return (
     <div className="border-b border-t border-rose-50/20 text-rose-50 py-4 flex items-center gap-1">
       <span className="text-2xl font-satoshi-regular">{title}</span>
     </div>
-  )
-}
+  );
+};
 
 export default LargeHeader;
